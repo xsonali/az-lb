527b18b25cab12695fbf510be555c006bdea9ce8
Gholam Alam <villshona@gmail.com>
2025-08-25 12:17:19 +1000

    Updated main.tf with Ubuntu image reference

--- main.tf
+++ main.tf
@@ -181,7 +181,7 @@ resource "azurerm_linux_virtual_machine" "vms_pool" {
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
+    offer     = "0001-com-ubuntu-server-jammy"
     sku       = "22_04-lts-gen2"
     version   = "latest"
   }
