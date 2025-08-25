bdc7442a663cc3f38d10421ab643e3fb832d44ba
Gholam Alam <villshona@gmail.com>
2025-08-25 11:33:20 +1000

    Update VM pool to Ubuntu 22.04 LTS Gen2 / Gen1 fallback

--- main.tf
+++ main.tf
@@ -182,7 +182,7 @@ resource "azurerm_linux_virtual_machine" "vms_pool" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "22_04-lts"
+    sku       = "22_04-lts-gen2"
     version   = "latest"
   }
 
